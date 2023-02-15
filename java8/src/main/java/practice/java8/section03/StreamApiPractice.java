992017959995c12e1160a29f2b6e283a014e1e2a
Kim-SuBin <sb2504@naver.com>
2023-02-16 01:28:21 +0900

    ✨Stream API 테스트를 위한 틀 구성

---
+++ java8/src/main/java/practice/java8/section03/StreamApiPractice.java
@@ -0,0 +1,24 @@
+package practice.java8.section03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamApiPractice {
+    public static void main(String[] args) {
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        List<OnlineClass> javaClasses = new ArrayList<>();
+        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
+        javaClasses.add(new OnlineClass(8, "The Java, 8 to 11", false));
+
+        List<List<OnlineClass>> onlineClasses = new ArrayList<>();
+        onlineClasses.add(springClasses);
+        onlineClasses.add(javaClasses);
+    }
+}
