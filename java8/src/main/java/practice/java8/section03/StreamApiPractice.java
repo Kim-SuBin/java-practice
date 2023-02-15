b1fb2db7cf4aa79091a3d0828429e750faf44827
Kim-SuBin <sb2504@naver.com>
2023-02-16 01:28:42 +0900

    ✨Stream API - 걸러내기

--- java8/src/main/java/practice/java8/section03/StreamApiPractice.java
+++ java8/src/main/java/practice/java8/section03/StreamApiPractice.java
@@ -2,6 +2,7 @@ package practice.java8.section03;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class StreamApiPractice {
     public static void main(String[] args) {
@@ -11,6 +12,19 @@ public class StreamApiPractice {
         springClasses.add(new OnlineClass(3, "spring mvc", false));
         springClasses.add(new OnlineClass(4, "spring core", false));
         springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        System.out.println("spring 으로 시작하는 수업");
+        springClasses.stream().filter(springClass -> springClass.getTitle().startsWith("spring"))
+                .forEach(springClass -> System.out.println(springClass.getId()));
+
+        System.out.println("close 되지 않은 수업");
+        // 방법 1. 람다 사용
+        springClasses.stream().filter(springClass -> !springClass.isClosed())
+                .forEach(springClass -> System.out.println(springClass.getId()));
+        // 방법 2. 메서드 레퍼런스와 static 메서드 사용
+        springClasses.stream().filter(Predicate.not(OnlineClass::isClosed))
+                .forEach(springClass -> System.out.println(springClass.getId()));
+
 
         List<OnlineClass> javaClasses = new ArrayList<>();
         javaClasses.add(new OnlineClass(6, "The Java, Test", true));
