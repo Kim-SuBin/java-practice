5602d599ee15cfad37f3bee140b4957a9f7fc24d
Kim-SuBin <sb2504@naver.com>
2023-02-26 19:07:27 +0900

    ✨Concurrent - Callable cancel() 설명 추가

--- java8/src/main/java/practice/java8/section06/CallableAndFuturePractice.java
+++ java8/src/main/java/practice/java8/section06/CallableAndFuturePractice.java
@@ -14,9 +14,9 @@ public class CallableAndFuturePractice {
 
         Future<String> future = executorService.submit(hello);
         System.out.println("Started!");
-        future.cancel(false);
-        future.get(); // get은 결과값이 오기를 기다림
-        System.out.println(future.isDone()); // 작업 상태 확인 (완료 true, 실패 false)
+        future.cancel(false); // true - 현재 진행 중인 작업 interrupt, false - 현재 진행 중인 작업 기다림
+//        future.get(); // get은 결과값이 오기를 기다림. 앞에 cancel이 있으면 exception 반환
+        System.out.println(future.isDone()); // 위에 cancel()이 수행되어도 종료된 것이기에 true 반환
         System.out.println("End"); // get에서 결과를 보낸 후 출력됨
 
         executorService.shutdown();
